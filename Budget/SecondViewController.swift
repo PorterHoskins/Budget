31494af825d9482c8c5607d951d502b1297147a9
phoskins <phoskins@apple.com>
2014-07-20 13:18:20 -0700

    Initial Commit

---
+++ Budget/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Budget
+//
+//  Created by Porter Hoskins on 7/20/14.
+//  Copyright (c) 2014 Lampo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
